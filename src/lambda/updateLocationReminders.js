61ab9a915e44b6f650fe9fa6d5b68541c8811e0b
thatguychrisw <thatguychrisw@gmail.com>
2021-04-18 15:55:25 -0400

    feat(updateLocationReminders): test adding publix

--- src/lambda/updateLocationReminders.js
+++ src/lambda/updateLocationReminders.js
@@ -1,20 +1,20 @@
 // /.netlify/functions/updateLocationReminders
 import got from 'got'
+import uuid from 'uuid'
 
 const createLocationReminders = (itemId) => {
   return [{
-    'type': 'reminder_add',
-    'temp_id': '7ad9609d-579f-4828-95c5-3600acdb2c81',
-    'uuid': '830cf409-daba-479c-a624-68eb0c07d01c',
-    'args': {
-      'item_id': itemId,
-      'service': 'email',
-      'type': 'location',
-      'name': 'Publix — Bluffs',
-      'loc_lat': '26.893893428777858',
-      'loc_long': '-80.0616267433865',
-      'loc_trigger': 'on_enter',
-      'radius': 100
+    "type": "reminder_add",
+    "uuid": uuid.v4(),
+    "args": {
+      "item_id": itemId,
+      "service": "email",
+      "type": "location",
+      "name": "Publix — Bluffs",
+      "loc_lat": "26.893893428777858",
+      "loc_long": "-80.0616267433865",
+      "loc_trigger": "on_enter",
+      "radius": 100
     }
   }]
 }
